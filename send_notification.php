641179d1ee3a35e3cf56739b2039adde1112e588
ruhulfbr <ruhul11bd@gmail.com>
2022-08-16 10:25:23 +0600

    First commit

--- send_notification.php
+++ send_notification.php
@@ -1,30 +1,31 @@
 <?php
-function sendNotification(){
-    $url ="https://fcm.googleapis.com/fcm/send";
 
-    $fields=array(
-        "to"=>$_REQUEST['token'],
-        "notification"=>array(
-            "body"=>$_REQUEST['message'],
-            "title"=>$_REQUEST['title'],
-            "icon"=>$_REQUEST['icon'],
-            "click_action"=>"https://google.com"
-        )
-    );
+    function sendNotification(){
+        $url ="https://fcm.googleapis.com/fcm/send";
 
-    $headers=array(
-        'Authorization: key=YOUR_SERVER_KEY',
-        'Content-Type:application/json'
-    );
+        $fields=array(
+            "to"=>$_REQUEST['token'],
+            "notification"=>array(
+                "body"=>$_REQUEST['message'],
+                "title"=>$_REQUEST['title'],
+                "icon"=>$_REQUEST['icon'],
+                "click_action"=>"https://google.com"
+            )
+        );
 
-    $ch=curl_init();
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_POST,true);
-    curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode($fields));
-    $result=curl_exec($ch);
-    print_r($result);
-    curl_close($ch);
-}
-sendNotification();
+        $headers=array(
+            'Authorization: key=YOUR_SERVER_KEY',
+            'Content-Type:application/json'
+        );
+
+        $ch=curl_init();
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_POST,true);
+        curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode($fields));
+        $result=curl_exec($ch);
+        print_r($result);
+        curl_close($ch);
+    }
+    sendNotification();
